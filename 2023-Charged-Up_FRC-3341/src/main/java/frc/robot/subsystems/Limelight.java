c05cdc3d0ac08d08c4707cd516eb78ac99c46421
Option 16 <westviewrobotics@gmail.com>
2023-01-21 17:09:44 -0800

    Added Pipelines in Limelight Subsystem

--- 2023-Charged-Up_FRC-3341/src/main/java/frc/robot/subsystems/Limelight.java
+++ 2023-Charged-Up_FRC-3341/src/main/java/frc/robot/subsystems/Limelight.java
@@ -11,82 +11,92 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 public class Limelight extends SubsystemBase {
+  /** Creates a new Limelight. */
 
-NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight-drswish");
+  NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight-oslime");
 
-private double txNum;
-private double tyNum;
-private double taNum;
-private int tvNum;
+  private double txNum;
+  private double tyNum;
+  private double taNum;
+  private int tvNum;
+  // Pipeline 0 - reflective tape
+  // Pipeline 1 to 8 - april tags
+  // Pipeline 9 - cone
+  // Pipeline 10 - cube 
+  public double ReflectiveTapePipeline = 0;
 
-    //This gets the tx, or the horizontal offset 
-    //from the crosshair in degrees (-27.0 to 27.0)
-    NetworkTableEntry tx = table.getEntry("tx"); 
+  // This gets the tx, or the horizontal offset
+  // from the crosshair in degrees (-27.0 to 27.0)
+  NetworkTableEntry tx = table.getEntry("tx");
 
-    //This gets the ty, or the vertical offset
-    //from the crosshair in degrees (-20.5 to 20.5)
-    NetworkTableEntry ty = table.getEntry("ty"); 
+  // This gets the ty, or the vertical offset
+  // from the crosshair in degrees (-20.5 to 20.5)
+  NetworkTableEntry ty = table.getEntry("ty");
 
-    //This gets the ta, or how much in % of the target
-    //is visible (0.0-100.0)
-    NetworkTableEntry ta = table.getEntry("ta"); 
+  // This gets the ta, or how much in % of the target
+  // is visible (0.0-100.0)
+  NetworkTableEntry ta = table.getEntry("ta");
 
-    //This gets the tv, which sees if the limelight 
-    //has a valid target (1) or no valid target (0)
-    NetworkTableEntry tv = table.getEntry("tv");
+  // This gets the tv, which sees if the limelight
+  // has a valid target (1) or no valid target (0)
+  NetworkTableEntry tv = table.getEntry("tv");
 
-double x = tx.getDouble(0.0);
-double y = ty.getDouble(0.0);
-double area = ta.getDouble(0.0);
-  
-  /** Creates a new ExampleSubsystem. */
+  double x = tx.getDouble(0.0);
+  double y = ty.getDouble(0.0);
+  double area = ta.getDouble(0.0);
+
   public Limelight() {
-
-    //We have to add these ports so that we can connect to 
-    //the limelight with our code through the robt's wifi
-    //PortForwarder.add(5800, "http://10.33.41.11:5801/", 5800);
+    // We have to add these ports so that we can connect to
+    // the limelight with our code through the robot's wifi
+    // PortForwarder.add(5800, "http://10.33.41.11:5801/", 5800);
     PortForwarder.add(5801, "http://limelight-oslime.local:5801", 5801);
     PortForwarder.add(5802, "http://limelight-oslime.local:5801", 5802);
     PortForwarder.add(5803, "http://limelight-oslime.local:5801", 5803);
     PortForwarder.add(5804, "http://limelight-oslime.local:5801", 5804);
     PortForwarder.add(5805, "http://limelight-oslime.local:5801", 5805);
     PortForwarder.add(5800, "http://limelight-oslime.local:5801", 5800);
-    
   }
-  
-  public double get_tx(){
+  public void changepipeline(int ReflectiveTapePipeline) {
+    NetworkTableInstance.getDefault().getTable("limelight").getEntry("getpipe").setNumber(ReflectiveTapePipeline);
+  }
+  public double get_tx() {
     return txNum;
   }
 
-  public double get_ty(){
+  public double get_ty() {
     return tyNum;
   }
 
-  public double get_ta(){
+  public double get_ta() {
     return taNum;
   }
 
-  public int get_tv(){
+  public int get_tv() {
     return tvNum;
   }
 
   @Override
   public void periodic() {
+    // This method will be called once per scheduler run
 
-     tx = table.getEntry("tx"); 
-     ty = table.getEntry("ty"); 
-     ta = table.getEntry("ta"); 
-     tv = table.getEntry("tv"); 
-    
+    tx = table.getEntry("tx");
+    ty = table.getEntry("ty");
+    ta = table.getEntry("ta");
+    tv = table.getEntry("tv");
+
     // tvNum can be either 1 or 0, so we instantiate by adding (int) in front
     // We will be assigning tvNum to the int (1 or 0) that limelight returns
-    tvNum = (int) tv.getDouble(0.0); 
+    tvNum = (int) tv.getDouble(0.0);
 
-        // We will be assigning tyNum to the double (-27.0 to 27.0) that limelight returns
+    // We will be assigning tyNum to the double (-27.0 to 27.0) that limelight
+    // returns
     txNum = tx.getDouble(0.0);
 
-    // We will be assigning tyNum to the double (-20.5 to 20.5) that limelight returns
+    // We will be assigning tyNum to the double (-20.5 to 20.5) that limelight
+    // returns
     tyNum = ty.getDouble(0.0);
 
     // We will be assigning taNum to the double (0.0-100.0) that limelight returns
@@ -98,15 +108,10 @@ double area = ta.getDouble(0.0);
     SmartDashboard.putNumber("LimelightY", y);
 
     // This will output the area of the target in SmartDashboard
-    SmartDashboard.putNumber("LimelightArea", area);  
-    
+    SmartDashboard.putNumber("LimelightArea", area);
+
     // This will output the value of the target in SmartDashboard (0 or 1)
-    SmartDashboard.putNumber("LimelightV", tvNum);  
+    SmartDashboard.putNumber("LimelightV", tvNum);
 
-  }
-
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
   }
 }
