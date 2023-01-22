076a34579d4d3efb2f2f227dc3dd455101070fdb
aric-volman <89823522+aric-volman@users.noreply.github.com>
2023-01-21 17:10:44 -0800

    Working Arm Subsystem and PivotArm Command
    Using cosine feedforward, the feedforward is calculated by multiplying a max holding voltage by the cosine or sine of the angle. We are using an integration range, so that PID only applies the Integral term nearing a couple degrees.

--- 2023-Charged-Up_FRC-3341/src/main/java/frc/robot/RobotContainer.java
+++ 2023-Charged-Up_FRC-3341/src/main/java/frc/robot/RobotContainer.java
@@ -7,12 +7,14 @@ package frc.robot;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.subsystems.*;
+import frc.robot.commands.*;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -24,13 +26,13 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  public final Arm arm = new Arm();
+  public final PivotArm turnToAngleTen = new PivotArm(arm, 10);
+  public final PivotArm turnToAngleThirty = new PivotArm(arm, 30);
+
+  public static final Joystick leftJoystick = new Joystick(0);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  private static Joystick joy = new Joystick(0);
-  private final Arm arm = new Arm();
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
@@ -46,16 +48,11 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
+    JoystickButton triggerTenDegree = new JoystickButton(leftJoystick, 1);
+    triggerTenDegree.onTrue(turnToAngleTen);
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
-  }
-  public static Joystick getJoy(){
-    return joy;
+    JoystickButton triggerThirtyDegree = new JoystickButton(leftJoystick, 5);
+    triggerThirtyDegree.onTrue(turnToAngleThirty);
   }
 
   /**
@@ -65,6 +62,10 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    return null;
+  }
+
+  public static Joystick getJoy1() {
+    return leftJoystick;
   }
 }
--- 2023-Charged-Up_FRC-3341/src/main/java/frc/robot/subsystems/Arm.java
+++ 2023-Charged-Up_FRC-3341/src/main/java/frc/robot/subsystems/Arm.java
@@ -5,23 +5,91 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.RobotContainer;
 
 public class Arm extends SubsystemBase {
-  TalonSRX armTalon = new TalonSRX(Constants.OperatorConstants.armPort);
-  /** Creates a new Arm. */
-  public Arm() {}
 
-  public void moveArm(double power){
-    armTalon.set(ControlMode.PercentOutput, power);
+  TalonSRX armTalon = new TalonSRX(Constants.OperatorConstants.armPort);
+
+  public PIDController armPID = new PIDController(0.012, 0.0001, 0);
+
+  public double angle = 0.0;
+
+  public boolean override = true;
+
+  /** Creates a new Arm. */
+  public Arm() {
+    resetEncoders();
+    armTalon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute);
+    //armTalon.setSensorPhase(true);
+    armTalon.setInverted(true);
+
+    // Very important - sets max output, so that PID doesn't output an output that it too high
+    armTalon.configPeakOutputForward(0.4);
+    armTalon.configPeakOutputReverse(-0.4);
+
+    armPID.setTolerance(0.5);
+    armPID.setIntegratorRange(-10, 10);
   }
+
+  // Tested 1/21/2022
+  // The testing arm holds with the power of the calculated feedforward, not the action (manual increment of 20%)
+  // This is calculated with sine, where the max voltage is 1.12/12 = 9% voltage
+  public void moveArm(double power){
+    armTalon.set(ControlMode.PercentOutput, power*0.2+(Math.sin(getAngle())*(1.12/12.0)));
+  }
+
+  // The robot starts at 0 degrees, and drops down to 90 degrees for the horizontal position
+  // Since sin(90) = 1, then we can multiply it by the max holding voltage
+  // Max holding voltage for testing pivot - 1.12 volts = 0.09 power
+  public void resetEncoders() {
+    armTalon.setSelectedSensorPosition(0, 0, 10);
+  }
+
+  public double getAngle() {
+    double result = (armTalon.getSelectedSensorPosition(0)/4096.0)*360.0;
+    return result;
+  }
+
+  public void updatePID() {
+    // Without two clamps: 1.12
+    // With clamps: 1.2
+    armTalon.set(ControlMode.PercentOutput, armPID.calculate(getAngle())+(Math.sin(getAngle())*(1.2/12.0)));
+    SmartDashboard.putNumber("PID: ", armPID.calculate(getAngle())+(Math.sin(getAngle())*(1.2/12.0)));
+  }
+
+  public boolean withinSetpoint() {
+    return armPID.atSetpoint();
+  }
+
+  public void setAngle(double angle) {
+    this.angle = angle;
+  }
+
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
-    moveArm(RobotContainer.getJoy().getY());
+    if (RobotContainer.getJoy1().getRawButtonReleased(2)) {
+      override = !override;
+    }
+
+    if (override) { // Auto code
+      moveArm(-1.0*RobotContainer.getJoy1().getY());
+      angle = 0.0;
+    } else if (!override) {
+      armPID.setSetpoint(angle);
+      updatePID();
+    }
+
+    SmartDashboard.putNumber("Ticks: ", armTalon.getSelectedSensorPosition(0));
+    SmartDashboard.putNumber("Voltage: ", armTalon.getMotorOutputPercent());
+    SmartDashboard.putNumber("Angle: ", getAngle());
+  
   }
 }
