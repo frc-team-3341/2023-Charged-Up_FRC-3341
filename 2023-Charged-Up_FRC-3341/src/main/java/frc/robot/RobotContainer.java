b408eba55ee59f6b296b32e2130a941aafcc21c6
Lucas Solomon <Lucassolomon05@gmail.com>
2023-03-20 16:43:54 -0700

    Update RobotContainer.java

--- 2023-Charged-Up_FRC-3341/src/main/java/frc/robot/RobotContainer.java
+++ 2023-Charged-Up_FRC-3341/src/main/java/frc/robot/RobotContainer.java
@@ -19,35 +19,37 @@ import frc.robot.subsystems.*;
  * This class is where the bulk of the robot should be declared. Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
  * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
-
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 
 
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
+  public static final Joystick joystick0 = new Joystick(0);
+  public static final Joystick joystick1 = new Joystick(1);
+  public static final Joystick joystick2 = new Joystick(2);
+  private final TankDrive tankDrive;
+  private static DriveTrain dt;
   
-  //We have to initialize these objects for the SpinToTarget, ProtoTurret, and AutoTurret commands
-  private final static Drivetrain drive = new Drivetrain();
-  private final static Limelight lime = new Limelight();
-  private final static LockOnTarget lock = new LockOnTarget(drive, lime, 0);
-  private final static CenterToTarget center = new CenterToTarget(lime, drive);
+  // The robot's subsystems and commands are defined here...
 
 
   public final Arm arm = new Arm();
   public final Claw claw = new Claw();
+  public final PoweredIntake poweredIntake = new PoweredIntake();
 
-  public static final Joystick leftJoystick = new Joystick(0);
-  public static final Joystick rightJoystick = new Joystick(1);
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  // final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+
+    configureBindings();
     // Configure the button bindings
-    joy1 = new Joystick(Constants.joy1);
-    joy2 = new Joystick(Constants.joy2);
-    configureButtonBindings();
+    dt = new DriveTrain();
+     tankDrive = new TankDrive(dt, joystick0, joystick1);
+    
+
   }
+
 
   /**
    * Use this method to define your button->command mappings. Buttons can be created by
@@ -56,23 +58,18 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
 
-
   private void configureBindings() {
-  
-    JoystickButton toTarget = new JoystickButton(joy1, 3);
-    toTarget.onTrue( new CenterToTarget(lime, drive));
-    
-    JoystickButton triggerStowPos = new JoystickButton(leftJoystick, Constants.ButtonMap.stowPosition);
+    JoystickButton triggerStowPos = new JoystickButton(joystick0, Constants.ButtonMap.stowPosition);
     triggerStowPos.onTrue(new Rotate(arm, 0));
 
-    JoystickButton triggerMiddlePos = new JoystickButton(leftJoystick, Constants.ButtonMap.middlePosition);
-    triggerMiddlePos.onTrue(new Rotate(arm, 10));
+    JoystickButton triggerMiddlePos = new JoystickButton(joystick0, Constants.ButtonMap.middlePosition);
+    triggerMiddlePos.onTrue(new Rotate(arm, 70+5));
 
-    JoystickButton triggerOtherPos = new JoystickButton(leftJoystick, Constants.ButtonMap.otherArbPosition);
-    triggerOtherPos.onTrue(new Rotate(arm, 30));
+    JoystickButton triggerTopPos = new JoystickButton(joystick0, Constants.ButtonMap.topPosition);
+    triggerTopPos.onTrue(new Rotate(arm,90+5));
 
-    JoystickButton triggerGroundPos = new JoystickButton(leftJoystick, Constants.ButtonMap.groundPosition);
-    triggerGroundPos.onTrue(new Rotate(arm, 90));
+    JoystickButton triggerGroundPos = new JoystickButton(joystick0, Constants.ButtonMap.groundPosition);
+    triggerGroundPos.onTrue(new Rotate(arm, 16+5));
 
     // JoystickButton triggerExt = new JoystickButton(leftJoystick, Constants.ButtonMap.fullyExtendedArm);
     // Extends 1 inch for testing
@@ -81,23 +78,44 @@ public class RobotContainer {
     // JoystickButton RestPosExt = new JoystickButton(leftJoystick, Constants.ButtonMap.restPositionArm);
     // RestPosExt.onTrue(new Extend(arm, 0));
 
-    JoystickButton triggerWristRight = new JoystickButton(rightJoystick, Constants.ButtonMap.wristRight);
-    triggerWristRight.onTrue(new SetWristPos(claw, 225));
+    
+    JoystickButton triggerWristRight = new JoystickButton(joystick1, Constants.ButtonMap.wristNinety);
+    triggerWristRight.onTrue(new SetWristPosPI(poweredIntake, -112.5));
 
-    JoystickButton triggerWristLeft = new JoystickButton(rightJoystick, Constants.ButtonMap.wristLeft);
-    triggerWristLeft.onTrue(new SetWristPos(claw, -225));
+    JoystickButton triggerWristLeft = new JoystickButton(joystick1, Constants.ButtonMap.wristOneEighty);
+    triggerWristLeft.onTrue(new SetWristPosPI(poweredIntake, -225));
 
-    JoystickButton triggerWristCenter = new JoystickButton(rightJoystick, Constants.ButtonMap.wristCenter);
-    triggerWristCenter.onTrue(new SetWristPos(claw, 0));
+    JoystickButton triggerWristCenter = new JoystickButton(joystick1, Constants.ButtonMap.wristCenter);
+    triggerWristCenter.onTrue(new SetWristPosPI(poweredIntake, 0));
 
-    JoystickButton triggerClawRest = new JoystickButton(rightJoystick, Constants.ButtonMap.clawRest);
-    triggerClawRest.onTrue(new SetClawPos(claw, 0));
+    /* 
+    JoystickButton triggerClawRest = new JoystickButton(joystick1, Constants.ButtonMap.clawCube); //changed from joystick1 to joystick2
+    triggerClawRest.onTrue(new SetClawPos(claw, 155/2)); //previously 155
+    JoystickButton triggerClawClosed = new JoystickButton(joystick1, Constants.ButtonMap.clawCone);
+    triggerClawClosed.onTrue(new SetClawPos(claw, 100/2)); //previously 100
+    JoystickButton triggerClawOpen = new JoystickButton(joystick1, Constants.ButtonMap.clawOpen);
+    triggerClawOpen.onTrue(new SetClawPos(claw, 245/2));
+   */
+   JoystickButton triggerStarClawRest = new JoystickButton(joystick1, Constants.ButtonMap.clawOpen);
+    triggerStarClawRest.onTrue(new SetPoweredClawPos(poweredIntake, Constants.Measurements.poweredIntakeOpenPinch));
 
-    JoystickButton triggerClawClosed = new JoystickButton(rightJoystick, Constants.ButtonMap.clawClosed);
-    triggerClawClosed.onTrue(new SetClawPos(claw, 50));
+    JoystickButton triggerStarClawClosed = new JoystickButton(joystick1, Constants.ButtonMap.clawCone);
+    triggerStarClawClosed.onTrue(new SetPoweredClawPos(poweredIntake, Constants.Measurements.poweredIntakeConePinch));
 
+    JoystickButton triggerStarClawClosedCanRotate = new JoystickButton(joystick1, Constants.ButtonMap.poweredIntakeConePinchCanRotate);
+    triggerStarClawClosedCanRotate.onTrue(new SetPoweredClawPos(poweredIntake, Constants.Measurements.poweredIntakeConePinchCanRotatePos));
+
+    JoystickButton triggerStarClawCube = new JoystickButton(joystick1, Constants.ButtonMap.clawCube);
+    triggerStarClawCube.onTrue(new SetPoweredClawPos(poweredIntake, Constants.Measurements.poweredIntakeCubePinch));
+
+    JoystickButton triggerFlywheelIn = new JoystickButton(joystick1, Constants.ButtonMap.flywheelIn);
+    triggerFlywheelIn.onTrue(new SetPoweredClawFlywheel(poweredIntake, 0.5));
+    triggerFlywheelIn.onFalse(new SetPoweredClawFlywheel(poweredIntake, 0.0));
+    
+    JoystickButton triggerFlywheelOut = new JoystickButton(joystick1, Constants.ButtonMap.flywheelOut);
+    triggerFlywheelOut.onTrue(new SetPoweredClawFlywheel(poweredIntake, -0.5));
+    triggerFlywheelOut.onFalse(new SetPoweredClawFlywheel(poweredIntake, 0.0));
   }
-
 
 
   /**
@@ -112,35 +130,18 @@ public class RobotContainer {
   }
 
   public static Joystick getJoy1() {
-    return leftJoystick;
+    return joystick0;
   }
 
   public static Joystick getJoy2() {
-    return rightJoystick;
+    return joystick1;
+}
+
+public static Joystick getJoy3() {
+  return joystick2;
 }
   
-   public static DriveTrain getDriveTrain(){
+public static DriveTrain getDriveTrain(){
     return dt;
-
-}
-  public static Joystick getJoy1(){
-    return joy1;
   }
-  public static Joystick getJoy2(){
-    return joy2;
-  }
-  public static Drivetrain getDrive(){
-    return drive;
-  }
-  public static Limelight getLime(){
-    return lime;
-  }
-
-  public static double get_tv() {
-    return 0;
-  }
-
-public static double get_ty() {
-    return 0;
-}
 }
